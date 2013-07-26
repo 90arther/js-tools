1ac49a1c2cc2a19c428ab329aeab394f5bbbf210
90Arther <90arther@gmail.com>
2013-07-26 11:20:51 +0800

    learn chrome_source
    learn chrome_source

---
+++ chrome_source/js/deom.js
@@ -0,0 +1,15 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 13-7-22
+ * Time: 下午4:13
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var a=1;
+var b=2;
+var c=3;
+var d= document.getElementById('point');
+ d= document.getElementsByTagName('p').innerHTML;
+var q=5;
